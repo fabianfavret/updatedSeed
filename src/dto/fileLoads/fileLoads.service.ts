ee221bbc8bdbb663b6cf581d9aecf5aa1bc9a012
Carlos Andrade <silveiradeandrade.carlos@gmail.com>
2023-06-08 10:55:13 -0300

    mejoras de manejo de los excel mejora de reportes

--- src/dto/fileLoads/fileLoads.service.ts
+++ src/dto/fileLoads/fileLoads.service.ts
@@ -26,153 +26,171 @@ export class FileLoadsService {
 
       //const dataJSON = [];
       const directoryPath = 'excel';
+      let errorOutputFilePath = '';
       let updatedOutputFilePath = '';
       let createdOutputFilePath = '';
 
       const files = fs.readdirSync(directoryPath);
       for (const file of files) {
-        this.logger.debug('file ---- ', file);
-        // const dataJSON = [];
-        let updatedConcatenatedString = '';
-        let createdConcatenatedString = '';
-        if (file.endsWith('.xlsx')) {
-          const filePath = `${directoryPath}/${file}`;
-          updatedOutputFilePath = `${directoryPath}/update_${file.split('.xlsx').join('.txt')}`;
-          createdOutputFilePath = `${directoryPath}/create_${file.split('.xlsx').join('.txt')}`;
+        if (file === '2_Primaria (Portuguese).xlsx') {
+          this.logger.debug('file ---- ', file);
+          // const dataJSON = [];
+          let errorConcatenatedString = '';
+          let updatedConcatenatedString = '';
+          let createdConcatenatedString = '';
+          if (file.endsWith('.xlsx')) {
+            const filePath = `${directoryPath}/${file}`;
+            errorOutputFilePath = `${directoryPath}/error_${file.split('.xlsx').join('.txt')}`;
+            updatedOutputFilePath = `${directoryPath}/update_${file.split('.xlsx').join('.txt')}`;
+            createdOutputFilePath = `${directoryPath}/create_${file.split('.xlsx').join('.txt')}`;
 
-          const sheetName = 'Seeds';
+            const sheetName = 'Seeds';
 
-          const workbook = xlsx.readFile(filePath); // Leer el archivo Excel
+            const workbook = xlsx.readFile(filePath); // Leer el archivo Excel
 
-          // const columnName = 'JSON'; //  nombre de la columna a leer
-          const xmlRowObject = xlsx.utils.sheet_to_json(workbook.Sheets[sheetName]); // Lee los datos de la hoja y convierte a un arreglo de objetos
+            // const columnName = 'JSON'; //  nombre de la columna a leer
+            const xmlRowObject = xlsx.utils.sheet_to_json(workbook.Sheets[sheetName]); // Lee los datos de la hoja y convierte a un arreglo de objetos
 
-          for (const row of xmlRowObject) {
-            // dataJSON.push({ id: JSON.parse(row[columnName]).id, data: row[columnName] });
-            this.logger.debug(`row----- ${row['ID con idioma']}`);
-            await _update({
-              $this: this,
-              token: params.token,
-              query: params.query,
-              id: row['ID con idioma'],
-              name: row['ID con idioma'],
-              data: row['JSON'],
-              node: row['ID'],
-              phase: String(row['Proceso']).toLowerCase()
-            });
-          }
-        }
-
-        async function _update(params: {
-          $this: FileLoadsService;
-          token: string;
-          query: {
-            educationYearGuid?: string;
-            educationDisciplineGuid?: string;
-            langCode?: string;
-            responsible?: string;
-          };
-          id: string;
-          name: string;
-          data: string;
-          node: string;
-          phase: string;
-        }) {
-          const seedData = await params.$this.get({
-            url: 'seeds/',
-            query: {
-              pageSize: '1000',
-              offset: '0',
-              jsonID: params.id,
-              orderBy: 'guid',
-              orderType: 'ASC'
-            },
-            accessToken: params.token
-          });
-
-          if (seedData.count > 0) {
-            //actualizar la semilla
-            for (const seed of seedData.loSeed) {
-              updatedConcatenatedString += seed.guid + '\n';
-
-              const guids = String(seed.guid).split('_');
-
-              let update = false;
-              switch (params.query.langCode) {
-                case 'PT':
-                  update = guids[0].indexOf('ES') < 0 && guids[0].indexOf('EN') < 0 && guids[0].indexOf('OCT-00') < 0 && guids[0].indexOf('TF-00') < 0;
-                  break;
-                case 'ES':
-                  update = guids[0].indexOf('ES') > -1 || guids[0].indexOf('OCT-00') > -1 || guids[0].indexOf('TF-00') > -1;
-                  break;
-                case 'EN':
-                  update = guids[0].indexOf('EN') > -1 || guids[0].indexOf('AL-00') > -1;
-                  break;
-              }
-
-              if (update) {
-                params.$this.logger.debug(`UPDATE seed----- ${seed.guid}`);
-                await params.$this.patch({
-                  url: 'seeds/{seedGuid}',
-                  path: { seedGuid: seed.guid },
-                  payload: { name: params.name, data: params.data },
-                  accessToken: params.token
+            for (const row of xmlRowObject) {
+              // dataJSON.push({ id: JSON.parse(row[columnName]).id, data: row[columnName] });
+              this.logger.debug(`row----- ${row['ID con idioma']}`);
+              try {
+                JSON.parse(row['JSON']);
+                await _update({
+                  $this: this,
+                  token: params.token,
+                  query: params.query,
+                  id: row['ID con idioma'],
+                  name: row['ID con idioma'],
+                  data: row['JSON'],
+                  node: row['ID'],
+                  phase: String(row['Proceso']).toLowerCase()
                 });
+              } catch (error) {
+                errorConcatenatedString += row['ID con idioma'] + '\n';
+                continue;
               }
             }
-          } else {
-            createdConcatenatedString += params.name + '\n';
-            const payload = {
-              name: params.name,
-              data: params.data,
-              status: 'done',
-              educationYearGuid: params.query.educationYearGuid,
-              educationDisciplineGuid: params.query.educationDisciplineGuid,
-              responsible: params.query.responsible,
-              langCode: params.query.langCode
+          }
+
+          async function _update(params: {
+            $this: FileLoadsService;
+            token: string;
+            query: {
+              educationYearGuid?: string;
+              educationDisciplineGuid?: string;
+              langCode?: string;
+              responsible?: string;
             };
-            //crear la semilla
-            //console.log('Crear semilla');
-            const nodes = await params.$this.get({
-              url: `nodes`,
-              query: { pageSize: '1000', offset: '0', search: `_${params.id}` },
+            id: string;
+            name: string;
+            data: string;
+            node: string;
+            phase: string;
+          }) {
+            const seedData = await params.$this.get({
+              url: 'seeds/',
+              query: {
+                pageSize: '1000',
+                offset: '0',
+                jsonID: params.id,
+                orderBy: 'guid',
+                orderType: 'ASC'
+              },
               accessToken: params.token
             });
-            if (nodes.count) {
-              for (const node of nodes.nodes) {
-                params.$this.logger.debug(`NEW seed----- ${node.guid}`);
+
+            if (seedData.count > 0) {
+              //actualizar la semilla
+              for (const seed of seedData.loSeed) {
+                updatedConcatenatedString += seed.guid + '\n';
+
+                const guids = String(seed.guid).split('_');
+
+                let update = false;
+                switch (params.query.langCode) {
+                  case 'PT':
+                    update = guids[0].indexOf('ES') < 0 && guids[0].indexOf('EN') < 0 && guids[0].indexOf('OCT-00') < 0 && guids[0].indexOf('TF-00') < 0;
+                    break;
+                  case 'ES':
+                    update = guids[0].indexOf('ES') > -1 || guids[0].indexOf('OCT-00') > -1 || guids[0].indexOf('TF-00') > -1;
+                    break;
+                  case 'EN':
+                    update = guids[0].indexOf('EN') > -1 || guids[0].indexOf('AL-00') > -1;
+                    break;
+                }
+
+                if (update) {
+                  params.$this.logger.debug(`UPDATE seed----- ${seed.guid}`);
+                  await params.$this.patch({
+                    url: 'seeds/{seedGuid}',
+                    path: { seedGuid: seed.guid },
+                    payload: { name: params.name, data: params.data },
+                    accessToken: params.token
+                  });
+                }
+              }
+            } else {
+              createdConcatenatedString += params.name + '\n';
+              const payload = {
+                name: params.name,
+                data: params.data,
+                status: 'done',
+                educationYearGuid: params.query.educationYearGuid,
+                educationDisciplineGuid: params.query.educationDisciplineGuid,
+                responsible: params.query.responsible,
+                langCode: params.query.langCode
+              };
+              //crear la semilla
+              //console.log('Crear semilla');
+              const nodes = await params.$this.get({
+                url: `nodes`,
+                query: { pageSize: '1000', offset: '0', search: `_${params.id}` },
+                accessToken: params.token
+              });
+              if (nodes.count) {
+                for (const node of nodes.nodes) {
+                  params.$this.logger.debug(`NEW seed----- ${node.guid}`);
+                  await params.$this.post({
+                    url: 'seeds',
+                    payload: {
+                      ...payload,
+                      nodeGuid: node.guid,
+                      phase: params.phase
+                    },
+                    accessToken: params.token
+                  });
+                }
+              } else {
+                params.$this.logger.debug(`NEW seed-----`);
                 await params.$this.post({
                   url: 'seeds',
-                  payload: {
-                    ...payload,
-                    nodeGuid: node.guid,
-                    phase: params.phase
-                  },
+                  payload: payload,
                   accessToken: params.token
                 });
               }
-            } else {
-              params.$this.logger.debug(`NEW seed-----`);
-              await params.$this.post({
-                url: 'seeds',
-                payload: payload,
-                accessToken: params.token
-              });
+              //buscar LO like
             }
-            //buscar LO like
           }
-        }
 
-        fs.writeFile(updatedOutputFilePath, updatedConcatenatedString, (error) => {
-          if (error) {
-            throw new Error(error.message);
-          }
-        });
-        fs.writeFile(createdOutputFilePath, createdConcatenatedString, (error) => {
-          if (error) {
-            throw new Error(error.message);
-          }
-        });
+          fs.writeFile(errorOutputFilePath, errorConcatenatedString, (error) => {
+            if (error) {
+              throw new Error(error.message);
+            }
+          });
+          fs.writeFile(updatedOutputFilePath, updatedConcatenatedString, (error) => {
+            if (error) {
+              throw new Error(error.message);
+            }
+          });
+          fs.writeFile(createdOutputFilePath, createdConcatenatedString, (error) => {
+            if (error) {
+              throw new Error(error.message);
+            }
+          });
+
+          this.logger.debug(`END ------ ${file}`);
+        }
       }
 
       return null;
